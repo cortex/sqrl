97ed42e7c9299c9a8e0b96073cc6bef000a243e4
Joakim Lundborg <joakim.lundborg@gmail.com>
2013-10-08 03:59:55 +0200

    Cleanup and testcase

--- sqrl.py
+++ sqrl.py
@@ -1,31 +1,64 @@
 import nacl.utils
 import nacl.signing
-from Crypto.Hash import SHA256
-from Crypto.Hash import HMAC
+
+import hashlib
+import hmac
 import base64
+import urllib.parse
 
-PRIVATE_KEY = b'2A~\xf8R\n,\r\x9b(\x0b\xc6\xd9\xae\xc4\xdd\x8b\xa07\xc1O\x8b\xdfg\x7f`\xc61\xb3&\xe2\xef'
+class Client:
+    def __init__(self, private_key):
+        self.private_key = private_key
 
-### CLIENT
-def make_key(private_key, site):
-	h = HMAC.new(private_key, digestmod=SHA256)
-	h.update(site)
-	return h.digest()
+    def make_key(self, site):
+        h = hmac.new(self.private_key, digestmod=hashlib.sha256)
+        h.update(bytes(site, "utf-8"))
+        return h.digest()
 
-def create_code(challenge, site):
-	site_key = make_key(PRIVATE_KEY, site)
-	signing_key = nacl.signing.SigningKey(site_key)
-	signed = signing_key.sign(challenge)
-	public_key = signing_key.verify_key
-	return public_key._key, signed
+    def create_code(self, sqrl):
+        parsed = urllib.parse.urlparse(sqrl)
+        site = parsed.netloc
+        encoded = urllib.parse.parse_qs(parsed.query)["sqrl"][0]
+        
+        challenge = base64.urlsafe_b64decode(encoded)
+        site_key = self.make_key(site)
+        signing_key = nacl.signing.SigningKey(site_key)
+        signed = signing_key.sign(bytes(challenge))
+        public_key = signing_key.verify_key
+        return public_key._key, signed
 
-### SERVER
-def verify_key(public_key, signature, challenge):
-	vk = nacl.signing.VerifyKey(public_key)
-	return challenge == vk.verify(signature)
+class ServerSession:
+    def __init__(self, site):
+        self.challenge = nacl.utils.random(32)
+        self.site = site
 
-challenge = nacl.utils.random(32)
-k,m = create_code(challenge, b"www.grc.com")
+    def get_sqrl(self):
+        return "sqrl://{}?sqrl={}".format(self.site,str(base64.urlsafe_b64encode(self.challenge), "utf-8"))
 
-t = verify_key(k, m, challenge)
-print(t)
+    def verify_key(self, public_key, signature):
+        vk = nacl.signing.VerifyKey(public_key)
+        try:
+            out = vk.verify(signature)
+        except nacl.signing.BadSignatureError:
+            return False
+        return self.challenge == out
+
+
+if __name__ == "__main__":
+    PRIVATE_KEY1 = b'correct horse battery staple'
+    PRIVATE_KEY2 = b'wrong horse battery staple'
+
+    client1 = Client(PRIVATE_KEY1)
+    client2 = Client(PRIVATE_KEY2)
+
+    session = ServerSession("www.grc.com")
+    sqrl_url = session.get_sqrl()
+
+    print (sqrl_url)
+
+    k1, m1 = client1.create_code(sqrl_url)
+    k2, m2 = client2.create_code(sqrl_url)
+
+    print("Verifying client1: ", session.verify_key(k1, m1))
+    print("Verifying client2: ", session.verify_key(k2, m2))
+    print("Verifying bad key: ", session.verify_key(k1, m2))
