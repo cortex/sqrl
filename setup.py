9e70e342e77c0e3441129c36f2c70dfc6358d1cf
Brian Pinkney <bushxnyc@gmail.com>
2013-10-15 01:05:44 -0400

    Added setup.py requirements.txt and LICENSE files and moved sqrl.py to it's own directory

---
+++ setup.py
@@ -0,0 +1,30 @@
+try:
+    from setuptools import setup
+except:
+    from disutils.core import setup
+
+dependencies = ['docopt', 'ed25519']
+
+setup(
+    name='sqrl',
+    version='0.0.1',
+    description='Command line SQRL client',
+    url='http://gitbub.com/bushxnyc/sqrl',
+    author='Brian Pinkney',
+    author_email='bushxnyc@gmail.com',
+    install_requires=dependencies,
+    packages=['sqrl'],
+    entry_points={
+        'console_script': [
+            'sqrl=sqrl.sqrl:main'
+        ],
+    },
+    classifiers=(
+        'Development Satus :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language ::  English',
+        'License ::  OSI Approved :: MIT License',
+        'Programming Language : Python',
+        'Programming Language : Pythoni :: 2.7',
+    )
+)
