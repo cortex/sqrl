6905e83a21a987083d0549ac63846ccefa359770
Brian Pinkney <bushxnyc@gmail.com>
2013-10-16 12:25:32 -0400

    Created client module to keep all SQRL specific logic in one place

--- sqrl/client/crypt.py
+++ sqrl/client/crypt.py
@@ -13,18 +13,16 @@ class Crypt:
     - Providing public key
     """
 
-    def __init__(self, masterkey, domain):
+    def __init__(self, masterkey):
         self.masterkey = masterkey
-        self.domain = domain
-        self.sk, self.vk = self._site_key_pair()
 
-    def _site_key_pair(self):
-        seed = self._site_seed()
+    def _site_key_pair(self, domain):
+        seed = self._site_seed(domain)
         sk = ed25519.SigningKey(seed)
         vk = sk.get_verifying_key()
         return sk, vk
 
-    def _site_seed(self):
+    def _site_seed(self, domain):
         """
         Generates a seed to based on the masterkey
         and the current site you authenicating with
@@ -33,13 +31,14 @@ class Crypt:
         """
         key = self.masterkey
         local_hmac = hmac.new(key)
-        local_hmac.update(self.domain)
+        local_hmac.update(domain)
         return local_hmac.hexdigest()
 
     def sign(self, value):
         signed = self.sk.sign(value)
         return baseconv.encode(signed)
 
-    def getPublicKey(self):
+    def getPublicKey(self, domain):
+        self.sk, self.vk = self._site_key_pair(domain)
         key = self.vk.to_bytes()
         return baseconv.encode(key)
--- sqrl/sqrl.py
+++ sqrl/sqrl.py
@@ -16,12 +16,8 @@ Example:
     sqrl "sqrl://example.com/login/sqrl?d=6&nut=a95fa8e88dc499758"
 """
 
-import pynotify
-from .test import test
-from .crypt import Crypt
+from client import Client
 from .mkm import MKM
-from .request import SQRLRequest
-from .parser import URLParser
 from docopt import docopt
 
 VERSION = "0.0.2"
@@ -35,40 +31,13 @@ def main():
 
 
 def run(url, bool_notify=False):
-    # Parse url
-    urlparsed = URLParser(url)
-    domain = urlparsed.getDomain()
-
     # Get MasterKey
     manager = MKM()
     masterkey = manager.get_key()
 
-    # Generate Site Specific Key pair
-    enc = Crypt(masterkey, domain)
-    public_key = enc.getPublicKey()
-
-    # Create Request Object
-    sqrlreq = SQRLRequest(urlparsed, public_key)
-    url = sqrlreq.url()
-
-    # Sign the url and send it
-    signed_url = enc.sign(url)
-    test(url, signed_url, public_key, domain)
-    result = sqrlreq.send(signed_url)
-
-    # notify of server response
-    if bool_notify:
-        if result:
-            notify("Authentication to " + domain + ": Successful")
-        else:
-            notify("Authentication to " + domain + ": Failed")
-
-
-def notify(msg):
-    pynotify.init("SQRL")
-    n = pynotify.Notification("pySQRL", msg)
-    n.show()
-
+    # Create sqrl client and submit request
+    sqrlclient = Client(masterkey, url)
+    sqrlclient.submit()
 
 if __name__ == "__main__":
     main()
